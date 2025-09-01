3ea9f6847293c292efc1891f9cd867fa46efc794
Prasanna <adnaikprasanna1055@gmail.com>
2025-09-01 20:58:32 +0530

    WORD-2 Task-B completed

--- wordplay_jira.py
+++ wordplay_jira.py
@@ -1,8 +1,12 @@
 def words_ending_in_ime(words):
     return [w for w in words if w.endswith("ime")]
 
+def words_with_rstlne(words):
+    return [w for w in words if any(c in w for c in "rstlne")]
+
 if __name__ == "__main__":
     with open("wordlist.txt") as f:
         wordlist = f.read().splitlines()
 
     print("a) Words ending in ime:", words_ending_in_ime(wordlist))
+    print("b) Words with r,s,t,l,n,e:", words_with_rstlne(wordlist))
