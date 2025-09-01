5f3bf340b71ae2bdca3ed9d5c32389747b010580
Prasanna <adnaikprasanna1055@gmail.com>
2025-09-01 23:57:15 +0530

    WJ-4 TASK_C COMPLETED

--- wordplay_jira.py
+++ wordplay_jira.py
@@ -4,9 +4,14 @@ def words_ending_in_ime(words):
 def words_with_rstlne(words):
     return [w for w in words if any(c in w for c in "rstlne")]
 
+def words_no_vowels(words):
+    vowels = "aeiou"
+    return [w for w in words if not any(c in vowels for c in w)]
+
 if __name__ == "__main__":
     with open("wordlist.txt") as f:
         wordlist = f.read().splitlines()
 
     print("a) Words ending in ime:", words_ending_in_ime(wordlist))
     print("b) Words with r,s,t,l,n,e:", words_with_rstlne(wordlist))
+    print("c) Words with no vowels:", words_no_vowels(wordlist))
